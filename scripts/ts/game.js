a185de495ddc79d139a908db41fa0692d330c237
Rob Simmons <robsimmons@gmail.com>
2020-04-19 00:36:19 -0400

    Make yeasty goodness... some things don't work right?

--- scripts/ts/game.js
+++ scripts/ts/game.js
@@ -4,78 +4,161 @@ var epochInMS = 1000;
 var jarVolume = 32;
 var filledVolume = 0;
 var spillage = 0;
-var Resource = /** @class */ (function () {
-    function Resource(incPerEpoch, initialAmount, decay) {
-        this.amount = initialAmount;
-        this.incPerEpoch = incPerEpoch;
-        this.decay = decay;
-        this.storedEpochs = 0;
-    }
-    ;
-    Resource.prototype.evolve = function (newEpochs) {
-        var totalEpochs = this.storedEpochs + newEpochs;
-        var amountToAdd = totalEpochs * this.incPerEpoch;
-        amountToAdd -= totalEpochs * this.decay;
-        if (amountToAdd >= 1 || amountToAdd <= -1) {
-            if (amountToAdd < 0) {
-                amountToAdd = Math.ceil(amountToAdd);
-            }
-            else {
-                amountToAdd = Math.floor(amountToAdd);
-            }
-            this.amount += amountToAdd;
-            if (this.amount < 0) {
-                this.amount = 0;
-            }
-            this.storedEpochs = 0;
-        }
-        else {
-            this.storedEpochs = totalEpochs;
-        }
-    };
-    return Resource;
-}());
-;
-var resourceStore = {
-    "yeast": new Resource(0, 0, 0),
-    "food": new Resource(0, 0, 0.5)
+// Misuse of the type, these are actually numbers 1-10
+var YEASTY_RATES = {
+    fed: 0.05,
+    happy: 0.05,
+    waiting: 0.01,
+    hungry: 0.01,
+    starving: 0.001,
+    dead: 0.0,
 };
+var YEASTY_HEALTH = {
+    fed: 1.0,
+    happy: 1.0,
+    waiting: 1.0,
+    hungry: 0.75,
+    starving: 0.25,
+    dead: 0.0,
+};
+var YEASTY_HUNGER = {
+    fed: 0.0,
+    happy: 0.1,
+    waiting: 0.5,
+    hungry: 0.9,
+    starving: 1.0,
+    dead: 0.0,
+};
+// Yeast volume multiplier
+var YEASTY_VOLUME = {
+    fed: 1.0,
+    happy: 1.75,
+    waiting: 2.5,
+    hungry: 1.75,
+    starving: 1.25,
+    dead: 1.0,
+};
+function hunger(yeast) {
+    var alive = yeast.fed + yeast.happy + yeast.waiting + yeast.hungry + yeast.starving;
+    var hungerAbsolute = yeast.fed * YEASTY_HUNGER.fed
+        + yeast.happy * YEASTY_HUNGER.happy
+        + yeast.waiting * YEASTY_HUNGER.waiting
+        + yeast.hungry * YEASTY_HUNGER.hungry
+        + yeast.starving * YEASTY_HUNGER.starving;
+    return hungerAbsolute / alive;
+}
+function stepYeast(yeast) {
+    var gotHappy = yeast.fed * YEASTY_RATES.fed;
+    var gotWaiting = yeast.happy * YEASTY_RATES.happy;
+    var gotHungry = yeast.waiting * YEASTY_RATES.waiting;
+    var gotStarving = yeast.hungry * YEASTY_RATES.hungry;
+    var gotDead = yeast.starving * YEASTY_RATES.starving;
+    return {
+        fed: yeast.fed - gotHappy,
+        happy: gotHappy + yeast.happy - gotWaiting,
+        waiting: gotWaiting + yeast.waiting - gotHungry,
+        hungry: gotHungry + yeast.hungry - gotStarving,
+        starving: gotStarving + yeast.starving - gotDead,
+        dead: gotDead + yeast.dead,
+    };
+}
+function feedYeast(amount, yeast) {
+    var feedMeSeymor = yeast.waiting + yeast.hungry + yeast.starving;
+    if (feedMeSeymor >= amount) {
+        // Not enough food to feed all the really hungry yeast
+        console.log("A");
+        return {
+            fed: yeast.fed + amount + amount,
+            happy: yeast.happy,
+            waiting: yeast.waiting - (yeast.waiting / feedMeSeymor) * amount,
+            hungry: yeast.hungry - (yeast.hungry / feedMeSeymor) * amount,
+            starving: yeast.starving - (yeast.starving / feedMeSeymor) * amount,
+            dead: yeast.dead,
+        };
+    }
+    else if (feedMeSeymor + yeast.happy >= amount) {
+        // Feeding all the really hungry yeast, plus some the yeast that could have a bite
+        console.log("B");
+        return {
+            fed: yeast.fed + amount + amount,
+            happy: yeast.happy - (amount - feedMeSeymor),
+            waiting: 0,
+            hungry: 0,
+            starving: 0,
+            dead: yeast.dead,
+        };
+    }
+    else {
+        // Some feed goes to waste as dead material
+        console.log("C");
+        return {
+            fed: (yeast.fed + yeast.happy + yeast.waiting + yeast.hungry + yeast.starving) * 2,
+            happy: 0,
+            waiting: 0,
+            hungry: 0,
+            starving: 0,
+            dead: yeast.dead + amount - (yeast.happy + yeast.waiting + yeast.hungry + yeast.starving),
+        };
+    }
+}
+function yeastVolume(yeast) {
+    return yeast.fed * YEASTY_VOLUME.fed
+        + yeast.happy * YEASTY_VOLUME.happy
+        + yeast.waiting * YEASTY_VOLUME.waiting
+        + yeast.hungry * YEASTY_VOLUME.hungry
+        + yeast.starving * YEASTY_VOLUME.starving
+        + yeast.dead * YEASTY_VOLUME.dead;
+}
+function clampYeast(maxVolume, yeast) {
+    var volume = yeastVolume(yeast);
+    if (volume <= maxVolume)
+        return [yeast, 0];
+    var loss = (volume - maxVolume) / volume;
+    return [
+        {
+            fed: yeast.fed * (1 - loss),
+            happy: yeast.happy * (1 - loss),
+            waiting: yeast.waiting * (1 - loss),
+            hungry: yeast.hungry * (1 - loss),
+            starving: yeast.starving * (1 - loss),
+            dead: yeast.dead * (1 - loss),
+        },
+        volume - maxVolume,
+    ];
+}
+var GLOBAL_YEAST = { fed: 1, happy: 0, waiting: 0, hungry: 0, starving: 0, dead: 0 };
 function render() {
     // Render the jar
     document.getElementById("jar-capacity").innerText = "" + jarVolume;
     document.getElementById("used").innerText = "" + filledVolume;
     document.getElementById("spillage").innerText = "" + spillage;
+    document.getElementById("used").innerText = "" + yeastVolume(GLOBAL_YEAST);
+    document.getElementById("hunger").innerText = Math.round(hunger(GLOBAL_YEAST) * 100) + "%";
     // For now, we assume all resources in stash are in div named stash-resourcename
-    for (var k in resourceStore) {
-        var elem = document.getElementById("stash-" + k);
+    /*
+      for (let k in resourceStore) {
+        const elem = document.getElementById("stash-" + k);
         if (elem != null) {
-            elem.innerText = "" + resourceStore[k].amount;
+          elem.innerText = `${resourceStore[k].amount}`;
         }
-    }
+      }
+    */
 }
 function evolveResources(epochs) {
-    if (epochs > 0) {
-        for (var k in resourceStore) {
-            var r = resourceStore[k];
-            r.evolve(epochs);
-        }
-        lastEpochMS += epochs * epochInMS;
+    for (var i = 0; i < epochs; i++) {
+        GLOBAL_YEAST = stepYeast(GLOBAL_YEAST);
     }
 }
 function gameLoop(event) {
     var epochDelta = createjs.Ticker.getTime(true) - lastEpochMS;
     var resourceEpochs = Math.floor(epochDelta / epochInMS);
     evolveResources(resourceEpochs);
-    // Update grow rate for yeast based on food
-    var foodVolume = resourceStore["food"].amount;
-    var yeastVolume = resourceStore["yeast"].amount / 10;
-    resourceStore["yeast"].incPerEpoch = foodVolume;
-    filledVolume = foodVolume;
-    filledVolume += yeastVolume;
-    if (filledVolume > jarVolume) {
-        spillage = filledVolume - jarVolume;
-        filledVolume = jarVolume;
+    if (resourceEpochs > 0) {
+        lastEpochMS += resourceEpochs * epochInMS;
     }
+    var _a = clampYeast(jarVolume, GLOBAL_YEAST), newYeast = _a[0], newSpill = _a[1];
+    GLOBAL_YEAST = newYeast;
+    spillage += newSpill;
     render();
 }
 ;
@@ -84,12 +167,15 @@ createjs.Ticker.addEventListener('tick', function (eventObj) {
     gameLoop(eventObj);
 });
 function addFood() {
-    if (filledVolume + 1 <= jarVolume) {
-        resourceStore["food"].amount++;
-    }
-    else {
-        console.log("Jar full!");
-    }
+    console.log("Adding food");
+    console.log(GLOBAL_YEAST);
+    GLOBAL_YEAST = feedYeast(1, GLOBAL_YEAST);
+    console.log(GLOBAL_YEAST);
+    var _a = clampYeast(jarVolume, GLOBAL_YEAST), newYeast = _a[0], newSpill = _a[1];
+    GLOBAL_YEAST = newYeast;
+    console.log(newYeast);
+    spillage += newSpill;
+    console.log(GLOBAL_YEAST);
 }
 window.onload = function () {
     // Add button click listeners
@@ -120,7 +206,7 @@ window.onload = function () {
     };
     var throwawayButton = document.getElementById("throwaway");
     throwawayButton.onclick = function () {
-        resourceStore["yeast"].amount = Math.ceil(resourceStore["yeast"].amount * 0.5);
-        resourceStore["food"].amount = Math.ceil(resourceStore["food"].amount * 0.5);
+        //    resourceStore["yeast"].amount = Math.ceil(resourceStore["yeast"].amount * 0.5)
+        //    resourceStore["food"].amount = Math.ceil(resourceStore["food"].amount * 0.5)
     };
 };
